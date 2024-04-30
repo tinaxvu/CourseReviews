eb345ba691c4157a1773262a78a961a3c87ff3d7
Wadie <ucu8tt@virginia.edu>
2024-04-29 21:51:41 -0400

    Created CourseReviewsSceneController

---
+++ src/main/java/edu/virginia/sde/reviews/CourseReviewsSceneController.java
@@ -0,0 +1,21 @@
+package edu.virginia.sde.reviews;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.Property;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.awt.*;
+import java.sql.SQLException;
+import java.util.List;
+public class CourseReviewsSceneController {
+}
