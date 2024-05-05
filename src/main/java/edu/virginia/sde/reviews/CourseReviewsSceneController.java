e6c31b14e71a345097cf4cda7a4ed1905742dcf7
Aiden Rocha <126905791+Arocha09@users.noreply.github.com>
2024-05-05 17:17:17 -0400

    Changes to make comment the rightmost column and that you can scroll to see whole comment

--- src/main/java/edu/virginia/sde/reviews/CourseReviewsSceneController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseReviewsSceneController.java
@@ -9,22 +9,19 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+
 import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 import javafx.scene.control.skin.TableColumnHeader;
+import javafx.util.Callback;
 
 public class CourseReviewsSceneController {
     @FXML
